2f497603b0ee5b4361149e95d40e2db968019fdc
SumeetSuley <83140479+SumeetSuley@users.noreply.github.com>
2022-04-13 20:19:03 +0530

    Add preliminary tests
    Tests for invalidity remaining

---
+++ test/rollapply.jl
@@ -0,0 +1,34 @@
+DATA_SIZE = 360
+index_timetype = Date(2000, 1,1) + Day.(0:(DATA_SIZE - 1))
+vec1 = randn(DATA_SIZE)
+vec2 = randn(DATA_SIZE)
+vec3 = randn(DATA_SIZE)
+df = DataFrame(Index = index_timetype,vec1 = vec1,vec2 =  vec2,vec3 =  vec3)
+ts = TS(df, 1)
+
+functions = [mean, median, sum, minimum, maximum, std]
+cols: [1, 3, :vec1, :vec3]
+windowsize = [1, 5, DATASIZE, 0, -1, 100000]
+
+function ts_test(func, col, windowsize)
+    return TSx.rollapply(func, ts, co, windowsize)
+end
+
+for fun in functions
+    @ts_test(fun, 1, 1).coredata == RollingFunctions.rolling(fun, df[!, 1], 1)
+end
+
+for fun in functions
+    @ts_test(fun, 3, 5).coredata == RollingFunctions.rolling(fun, df[!, 3], 5)
+end
+
+for fun in functions
+    @ts_test(fun, :vec1, 5).coredata == RollingFunctions.rolling(fun, df[!, :vec1], 5)
+end
+
+for fun in functions
+    @ts_test(fun, :vec3, DATA_SIZE).coredata == RollingFunctions.rolling(fun, df[!, :vec3], DATA_SIZE)
+end
+
+
+
