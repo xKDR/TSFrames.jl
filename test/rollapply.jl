b787e3d9cb8f1be158842b9105cd440e2a4581de
SumeetSuley <83140479+SumeetSuley@users.noreply.github.com>
2022-04-14 02:25:23 +0530

    revised/updated all the functions

--- test/rollapply.jl
+++ test/rollapply.jl
@@ -3,20 +3,58 @@ index_timetype = Date(2000, 1,1) + Day.(0:(DATA_SIZE - 1))
 vec1 = randn(DATA_SIZE)
 vec2 = randn(DATA_SIZE)
 vec3 = randn(DATA_SIZE)
-df = DataFrame(Index = index_timetype,vec1 = vec1,vec2 =  vec2,vec3 =  vec3)
+df = DataFrame(Index = index_timetype, vec1 = vec1,vec2 = vec2, vec3 = vec3)
 ts = TS(df, 1)
 
 functions = [mean, median, sum, minimum, maximum, std]
-cols: [1, 3, :vec1, :vec3]
-windowsize = [1, 100, DATASIZE]
+cols = [2, 3, :vec1, :vec3]          # col 1 is index
+windowsize = [1, 5, 100, DATASIZE]
 
-function ts_test(func, col, windowsize)
-    return TSx.rollapply(func, ts, co, windowsize)
+@test typeof(TSx.rollapply(mean, ts, 2, 5)) == TSx.TS
+
+for fun in functions
+    res = @test TSx.rollapply(fun, ts, 2, 5).coredata[!, 2] == RollingFunctions.rolling(fun, df[!, 2], 5)
+    print(res)
 end
+
+for fun in functions
+    res = @test TSx.rollapply(fun, ts, :vec1, 5).coredata[!, :vec1] == RollingFunctions.rolling(fun, df[!, :vec1], 5)
+    print(res)
+end
+
+for fun in functions
+    res = @test TSx.rollapply(fun, ts, :vec3, 100).coredata[!, :vec3] == RollingFunctions.rolling(fun, df[!, :vec3], 100)
+    print(res)
+end
+
+for fun in functions
+    res = @test TSx.rollapply(fun, ts, :vec3, DATA_SIZE).coredata[!, :vec3] == RollingFunctions.rolling(fun, df[!, :vec3], DATA_SIZE)
+    print(res)
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 for fun in functions
     @ts_test(fun, 1, 1).coredata[!, 2] == RollingFunctions.rolling(fun, df[!, 1], 1)
 end
+
 
 for fun in functions
     @ts_test(fun, 3, 100).coredata[!,2] == RollingFunctions.rolling(fun, df[!, 3], 100)
