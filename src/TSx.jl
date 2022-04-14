87135a7c763fce8c83919755e845880f69a2ae31
Chirag Anand <anand.chirag@gmail.com>
2022-04-14 17:21:08 +0530

    fix typo

--- src/TSx.jl
+++ src/TSx.jl
@@ -136,7 +136,7 @@ end
 function TS(coredata::DataFrame)
     if "Index" in names(coredata)
         return TS(coredata, :Index)
-    elif DataFrames.ncol(coredata) == 1
+    elseif DataFrames.ncol(coredata) == 1
         return TS(coredata, collect(1:nrow(coredata)))
     else
         return TS(coredata, 1)
