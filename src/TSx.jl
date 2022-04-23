7cd101f4f1edbabac543db7bbfb98e1a071f803c
Chirag Anand <anand.chirag@gmail.com>
2022-04-23 23:10:26 +0530

    add convert to Matrix method

--- src/TSx.jl
+++ src/TSx.jl
@@ -23,7 +23,9 @@ export TS,
     JoinAll,
     JoinLeft,
     JoinRight,
+    Matrix,
     apply,
+    convert,
     cbind,
     describe,
     diff,
@@ -361,7 +363,7 @@ data types of columns.
 # Examples
 ```jldoctest; setup = :(using TSx, DataFrames, Dates, Random, Statistics)
 julia> using Random;
-julia> random(x) = rand(MersenneTwister(123), x..);
+julia> random(x) = rand(MersenneTwister(123), x...);
 julia> ts = TS(random(([1, 2, 3, 4, missing], 10)))
 julia> describe(ts)
 ```
@@ -386,6 +388,64 @@ function _convert(::Type{String}, date::Date)
     Dates.format(date, "yyyy-mm-dd")
 end
 
+"""
+# Conversion of non-Index data to Matrix
+
+```jldoctest; setup = :(using TSx, DataFrames, Dates, Random, Statistics)
+julia> using Random;
+julia> random(x) = rand(MersenneTwister(123), x);
+julia> ts = TS([random(10) random(10)])
+julia> show(ts)
+(10 x 2) TS with Int64 Index
+
+ Index  x1         x2        
+ Int64  Float64    Float64   
+─────────────────────────────
+     1  0.768448   0.768448
+     2  0.940515   0.940515
+     3  0.673959   0.673959
+     4  0.395453   0.395453
+     5  0.313244   0.313244
+     6  0.662555   0.662555
+     7  0.586022   0.586022
+     8  0.0521332  0.0521332
+     9  0.26864    0.26864
+    10  0.108871   0.108871
+
+julia> Matrix(ts)
+10×2 Matrix{Float64}:
+ 0.768448   0.768448
+ 0.940515   0.940515
+ 0.673959   0.673959
+ 0.395453   0.395453
+ 0.313244   0.313244
+ 0.662555   0.662555
+ 0.586022   0.586022
+ 0.0521332  0.0521332
+ 0.26864    0.26864
+ 0.108871   0.108871
+
+julia> convert(Matrix, ts)
+10×2 Matrix{Float64}:
+ 0.768448   0.768448
+ 0.940515   0.940515
+ 0.673959   0.673959
+ 0.395453   0.395453
+ 0.313244   0.313244
+ 0.662555   0.662555
+ 0.586022   0.586022
+ 0.0521332  0.0521332
+ 0.26864    0.26864
+ 0.108871   0.108871
+
+"""
+function Base.convert(::Type{Matrix}, ts::TS)
+    Matrix(ts.coredata[!, Not(:Index)])
+end
+
+function (Matrix)(ts::TS)
+    convert(Matrix, ts)
+end
 
 """
 # Subsetting/Indexing
