b78c8e8604be9d4131e8ad9e3da80e5b4f5512a2
Chirag Anand <anand.chirag@gmail.com>
2022-08-06 16:09:24 +0530

    returning correct output now, refs #51

--- src/endpoints.jl
+++ src/endpoints.jl
@@ -1,13 +1,23 @@
+"""
+# Computing end points
+```julia
+endpoints(ts::TS, on::T, k::Int=1) where {T<:Union{Symbol, String}}
+endpoints(ts::TS, on::Function, k::Int=1)
+```
+
+Return index values for last observation in `ts` for the period given
+by `on` every `k` instance.
+"""
 function endpoints(ts::TS, on::T, k::Int=1) where {T<:Union{Symbol, String}}
-    if (on == :days)
+    if (on == :days || on == "days")
         endpoints(ts, i -> Dates.yearmonthday.(i), k)
-    elseif (on == :weeks)
+    elseif (on == :weeks || on == "weeks")
         endpoints(ts, i -> Dates.week.(i), k)
-    elseif (on == :months)
+    elseif (on == :months || on == "months")
         endpoints(ts, i -> Dates.yearmonth.(i), k)
-    elseif (on == :quarters)
+    elseif (on == :quarters || on == "quarters")
         endpoints(ts, i -> [(year(x), Dates.quarterofyear(x)) for x in i], k)
-    elseif (on == :years)
+    elseif (on == :years || on == "years")
         endpoints(ts, i -> Dates.year.(i), k)
     else
         error("unsupported value supplied to `on`")
@@ -19,6 +29,6 @@ function endpoints(ts::TS, on::Function, k::Int=1)
     ex = Expr(:call, on, ii)
     new_index = eval(ex)
     new_index_unique = unique(new_index)
-    points = new_index_unique[1:k:length(new_index_unique)]
-    findall(x -> x in points, new_index)
+    points = new_index_unique[k:k:length(new_index_unique)]
+    [findlast([p] .== new_index) for p in points]
 end
