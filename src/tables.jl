8040b9d248a51a02d0e55b455fd0b733e0493c81
Siddhant Chaudhary <urssidd@gmail.com>
2022-10-06 10:55:22 +0530

    Added more `Tables.jl` function implementations; all the work is delegated to DataFrames.jl.

--- src/tables.jl
+++ src/tables.jl
@@ -6,3 +6,17 @@ Tables.rows(ts::TS) = DataFrames.eachrow(ts.coredata)
 Tables.columnaccess(::Type{TS}) = true
 Tables.columns(ts::TS) = DataFrames.eachcol(ts.coredata)
 
+Tables.rowtable(ts::TS) = Tables.rowtable(ts.coredata)
+Tables.columntable(ts::TS) = Tables.columntable(ts.coredata)
+
+Tables.namedtupleiterator(ts::TS) = Tables.namedtupleiterator(ts.coredata)
+
+Tables.columnindex(ts::TS, idx::Symbol) = Tables.columnindex(ts.coredata, idx)
+Tables.columnindex(ts::TS, idx::String) = Tables.columnindex(ts.coredata, Symbol(idx))
+
+Tables.schema(ts::TS) = Tables.schema(ts.coredata)
+
+Tables.materializer(::Type{<:TS}) = TS
+
+Tables.getcolumn(ts::TS, i::Int) = Tables.getcolumn(ts.coredata, i)
+Tables.getcolumn(ts::TS, nm::Symbol) = Tables.getcolumn(ts.coredata, nm)
