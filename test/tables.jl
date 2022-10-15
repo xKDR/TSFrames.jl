ebb689ea530be829d4bc11be265d3621f1ac7a13
Siddhant Chaudhary <urssidd@gmail.com>
2022-10-15 22:37:28 +0530

    Adding tests for `Tables.rowtable` and `Tables.columntable`.

--- test/tables.jl
+++ test/tables.jl
@@ -1,12 +1,36 @@
 dates = Date(2022, 1, 1):Day(1):Date(2022, 1, 15)
-ts = TS(DataFrame(Index=dates), 1:15)
+ts = TS(DataFrame(Index=dates, x1=1:15))
 
 # testing Tables.rows
 for row in Tables.rows(ts)
-    @test day(row[:Index] == row[:x1])
+    @test day(row[:Index]) == row[:x1]
 end
 
 # testing Tables.columns
 for col in Tables.columns(ts)
     @test length(col) == 15
+end
+
+# testing Tables.rowtable
+rowTable = Tables.rowtable(ts) 
+@test typeof(rowTable) == Vector{NamedTuple{(:Index, :x1), Tuple{Date, Int64}}}
+for i in 1:15
+    date = rowTable[i][:Index]
+    @test year(date) == 2022
+    @test month(date) == 1
+    @test day(date) == i
+
+    @test rowTable[i][:x1] == day(date)
+end
+
+# testing Tables.columntable
+columnTable = Tables.columntable(ts)
+@test typeof(columnTable) == NamedTuple{(:Index, :x1), Tuple{Vector{Date}, Vector{Int64}}}
+for i in 1:15
+    date = columnTable[:Index][i]
+    @test year(date) == 2022
+    @test month(date) == 1
+    @test day(date) == i
+
+    @test columnTable[:x1][i] == day(date)
 end
