c4ba6d038ce0289c2acca7eb31edf1e1d4804d14
Siddhant Chaudhary <urssidd@gmail.com>
2022-10-18 12:43:14 +0530

    Only keeping relevant testcases for `lag`.

--- test/lag.jl
+++ test/lag.jl
@@ -1,16 +1,16 @@
 # constants
-date_from = Date(2022, 1, 1)
-date_to = Date(2022, 5, 31)
-dates = date_from:Day(1):date_to
+DATA_SIZE = 360
+dates = Date(2000, 1,1) + Day.(0:(DATA_SIZE - 1))
 
-ts = TS(rand(length(dates)), dates)
+ts = TS(rand(1:100, DATA_SIZE), dates)
 
-for lagby in 1:length(ts)
+# lagging by something atmost DATA_SIZE
+for lagby in [0, floor(Int(DATA_SIZE/2)), DATA_SIZE]
     lagged_ts = lag(ts, lagby)
 
     # test that Index remains the same
     @test isequal(lagged_ts[:, :Index], ts[:, :Index])
-    
+
     # first lagby values are missing
     @test isequal(Vector{Missing}(lagged_ts[1:lagby, :x1]), fill(missing, lagby))
 
@@ -18,3 +18,7 @@ for lagby in 1:length(ts)
     isequal(lagged_ts[(lagby + 1):length(ts), :x1], ts[1:(length(ts) - lagby), :x1])
 end
 
+# lagging by something greater than DATA_SIZE
+lagged_ts = lag(ts, DATA_SIZE + 5)
+@test isequal(lagged_ts[:, :Index], ts[:, :Index])
+@test isequal(Vector{Missing}(lagged_ts[1:DATA_SIZE, :x1]), fill(missing, DATA_SIZE))
