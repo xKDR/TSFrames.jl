3a443b08baef6c87c3b91e53b92556390ff67c8d
Shikhar Mishra <sm_data@outlook.com>
2023-03-23 14:16:26 +0530

    Update src/indicators.jl
    Co-authored-by: Anshul Singhvi <anshulsinghvi@gmail.com>

--- src/indicators.jl
+++ src/indicators.jl
@@ -1,4 +1,4 @@
 # Methods for porting Indicators.jl functions to TSFrame objects from TSFrames.jl package
 # See their respective documentation on Indicators.jl
-runmean(X::TSFrame, x::Symbol; args...) = TSFrame(Indicators.runmean(X[:,x]; args...))
-sma(X::TSFrame, x::Symbol; args...) = TSFrame(Indicators.sma(X[:,x]; args...))
+Indicators.runmean(X::TSFrame, x::Symbol; kwargs...) = TSFrame(Indicators.runmean(X[:,x]; kwargs...))
+Indicators.sma(X::TSFrame, x::Symbol; kwargs...) = TSFrame(Indicators.sma(X[:,x]; kwargs...))
